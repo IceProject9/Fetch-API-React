17c896ceda969873719a694f7721f8e106069d6e
Stepan <magnum38magnum123@gmail.com>
2021-05-23 00:58:31 +0300

    1

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-double-ring-c0fqcc3zi1f">
+        <div class="ldio-s0vpin46yi">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner
+
+
